bf8adc79f1fec6fb7b2b7776c03b1bddd1d39a7a
Clifford Oh <ohcl@oregonstate.edu>
2024-04-09 13:55:18 -0400

    part1 courseinfo exercises 1.3-1.5 changes

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
@@ -19,9 +19,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part content={props.parts[0].content} exercises={props.parts[0].exercises} />
-      <Part content={props.parts[1].content} exercises={props.parts[1].exercises} />
-      <Part content={props.parts[2].content} exercises={props.parts[2].exercises} />
+      <Part content={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part content={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part content={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   )
 }
@@ -35,23 +35,30 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { content: 'Fundamentals of React', exercises: 10 },
-    { content: 'Using props to pass data', exercises: 7 },
-    { content: 'State of a component', exercises: 14 }
-  ];
-
-  let total_exercises = 0;
-  for (let i = 0; i < parts.length; i++) {
-    total_exercises += parts[i].exercises;
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
+  const total_exercises = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
+      <Content parts={course.parts} />
       <Total total={total_exercises} />
     </div>
   )
